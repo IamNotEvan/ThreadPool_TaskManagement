3f87d1c624a20968b3127e039d2146596e15109d
Bugeon Lee <117389205+IamNotEvan@users.noreply.github.com>
2023-03-25 17:21:05 -0400

    Add files via upload

--- threadpool.c
+++ threadpool.c
@@ -12,6 +12,7 @@
 #include "threadpool.h"
 #include <strings.h>
 #include <stdio.h>
+#include <threads.h>
 
 // #include <threadpool.h>
 /*
@@ -31,14 +32,11 @@ enum future_status
 struct thread_pool
 {
     struct list global_q;
-    struct list waiting_q;
     struct worker_thread *threads;
-    //int global_q_size;
     bool is_shutdown;
     int num_threads;
-    int count;
+
     pthread_cond_t global_queue_not_empty;
-    // pthread_cond_t global_queue_not_full;
     pthread_mutex_t global_lock;
     pthread_t *thread_array;
     pthread_barrier_t readysetgo;
@@ -54,7 +52,6 @@ struct future
     void *data;
     void *result;
     bool is_ready;
-    pthread_mutex_t task_lock;
     pthread_cond_t task_ready;
     enum future_status status;
 };
@@ -62,66 +59,38 @@ struct future
 struct worker_thread
 {
     pthread_mutex_t local_lock;
-    struct list_elem elem;
     struct list local_q;
     int local_q_size;
     int id;
-
     struct thread_pool *pool;
-    bool is_stopped;
 };
 
-// Important Notes
-// A flag should denote when the thread pool is shutting down.
-//  A static function should be created to perform the core work
-//  of each worker thread. The thread pool submit() function should
-//  allocate a new future and submit it to the pool. The get()
-//  function may require the calling thread to help complete the
-//  future being joined. The shutdown and destroy() function should
-// shut down the thread pool, join all worker threads, and deallocate
-// memory allocated on behalf of the worker threads. The free()
-//  function should be used by the client to free memory for a
-// future instance allocated in thread pool submit(). Atomic
-//       updates should be used to ensure the state of a future is
-//        updated correctly and to avoid conflicts between worker
-//         threads when stealing or helping with a future.
-
-static pthread_barrier_t readysetgo;
-/*
-struct thread_pool *thread_pool_new(int nthreads);
-void thread_pool_shutdown_and_destroy(struct thread_pool *pool);
-typedef void *(*fork_join_task_t)(struct thread_pool *pool, void *data);
-struct future *thread_pool_submit(struct thread_pool *pool, fork_join_task_t task, void *data);
-void *future_get(struct future *);
-void future_free(struct future *);
-*/
 static void *worker_function(void *_arg);
+static _Thread_local struct worker_thread *locality = NULL;
 
 /* Create a new thread pool with no more than n threads. */
 struct thread_pool *thread_pool_new(int nthreads)
 {
     struct thread_pool *pool = malloc(sizeof(*pool));
     pool->num_threads = nthreads;
-    pool->threads = malloc(nthreads * sizeof(struct worker_thread));
     pool->thread_array = malloc(nthreads * sizeof *pool->thread_array);
     pool->is_shutdown = false;
-    pool->count = 0;
-    //pool->global_q_size = 0;
+    pthread_barrier_init(&pool->readysetgo, NULL, nthreads);
     list_init(&pool->global_q);
-    list_init(&pool->waiting_q);
+    pool->threads = malloc(nthreads * sizeof *pool->threads);
 
     pthread_mutex_init(&pool->global_lock, NULL);
     pthread_cond_init(&pool->global_queue_not_empty, NULL);
-    pthread_cond_init(&pool->global_queue_not_empty, NULL);
-
-    pthread_barrier_init(&readysetgo, NULL, nthreads);
 
     for (int i = 0; i < nthreads; i++)
     {
-        printf("created thread \n");
-        pthread_create(&pool->thread_array[i], NULL, worker_function, pool);
+        pool->threads[i].pool = pool;
+        cpu_set_t mask;
+        CPU_ZERO(&mask);
+        CPU_SET(i, &mask);
+        pthread_create(&pool->thread_array[i], NULL, worker_function, &pool->threads[i]);
+        pthread_setaffinity_np(pool->thread_array[i], sizeof(mask), &mask);
     }
-
     return pool;
 }
 
@@ -134,13 +103,13 @@ struct thread_pool *thread_pool_new(int nthreads)
  */
 void thread_pool_shutdown_and_destroy(struct thread_pool *pool)
 {
+
     // Wake up all threads to handle the shutdown
-        pthread_mutex_lock(&pool->global_lock);
-        pool->is_shutdown = true;
-        pthread_cond_broadcast(&pool->global_queue_not_empty);
-        pthread_mutex_unlock(&pool->global_lock);
+    pthread_mutex_lock(&pool->global_lock);
+    pool->is_shutdown = true;
 
-
+    pthread_cond_broadcast(&pool->global_queue_not_empty);
+    pthread_mutex_unlock(&pool->global_lock);
     // Join all threads and free resources
     for (int i = 0; i < pool->num_threads; i++)
     {
@@ -148,10 +117,15 @@ void thread_pool_shutdown_and_destroy(struct thread_pool *pool)
         {
             perror("Failed to join");
         }
+       
     }
-    printf("here\n");
 
-    pthread_barrier_destroy(&readysetgo);
+    pthread_cond_destroy(&pool->global_queue_not_empty);
+    pthread_mutex_destroy(&pool->global_lock);
+    pthread_barrier_destroy(&pool->readysetgo);
+    free(pool->thread_array);
+    free(pool->threads);
+    free(pool);
 }
 
 /*
@@ -175,13 +149,22 @@ struct future *thread_pool_submit(
     future_task->data = data;
     future_task->pool = pool;
     pthread_cond_init(&future_task->task_ready, NULL);
-    pthread_mutex_init(&future_task->task_lock, NULL);
 
-    pthread_mutex_lock(&pool->global_lock);
-    list_push_back(&pool->global_q, &future_task->elem);
-    //pool->global_q_size++;
-    pthread_cond_signal(&pool->global_queue_not_empty);
-    pthread_mutex_unlock(&pool->global_lock);
+    if (locality == NULL)
+    {
+        pthread_mutex_lock(&pool->global_lock);
+        list_push_back(&pool->global_q, &future_task->elem);
+        pthread_cond_signal(&pool->global_queue_not_empty);
+        pthread_mutex_unlock(&pool->global_lock);
+    }
+    else
+    {
+        pthread_mutex_lock(&locality->local_lock);
+        list_push_back(&locality->local_q, &future_task->elem);
+        pthread_cond_signal(&pool->global_queue_not_empty);
+        pthread_mutex_unlock(&locality->local_lock);
+    }
+
     return future_task;
 }
 
@@ -192,33 +175,63 @@ struct future *thread_pool_submit(
  */
 void *future_get(struct future *task)
 {
-    if (task->status == PROGRESS)
-    {
-        pthread_cond_wait(&task->task_ready, &task->task_lock);
-    }
-    else if (task->status == START)
+    // pthread_mutex_lock(&locality->local_lock);
+    if (locality == NULL)
     {
         pthread_mutex_lock(&task->pool->global_lock);
-        if (!list_empty(&task->pool->global_q))
+        if (task->status == PROGRESS)
         {
-            
-            list_pop_front(&task->pool->global_q);
-            //task->pool->global_q_size--;
-           
+            while (task->status != COMPLETED)
+            {
+                pthread_cond_wait(&task->task_ready, &task->pool->global_lock);
+            }
+        }
+        else if (task->status == START)
+        {
+
+            list_remove(&task->elem);
+
+            task->status = PROGRESS;
+
+            pthread_mutex_unlock(&task->pool->global_lock);
+            void *res = task->func(task->pool, task->data);
+            pthread_mutex_lock(&task->pool->global_lock);
+
+            task->result = res;
+            task->status = COMPLETED;
+            pthread_cond_signal(&task->task_ready);
         }
         pthread_mutex_unlock(&task->pool->global_lock);
-        task->status = PROGRESS;
-
-        void *res = task->func(task->pool, task->data);
-
-        //pthread_mutex_lock(&task->pool->global_lock);
-        task->result = res;
-        task->status = COMPLETED;
-        pthread_cond_signal(&task->task_ready);
-
-        //pthread_mutex_unlock(&task->pool->global_lock);
+        return task->result;
     }
+    else
+    {
+        pthread_mutex_lock(&locality->local_lock);
+        if (task->status == PROGRESS)
+        {
+            while (task->status != COMPLETED)
+            {
+                pthread_cond_wait(&task->task_ready, &locality->local_lock);
+            }
+        }
+        else if (task->status == START)
+        {
 
+            list_remove(&task->elem);
+
+            task->status = PROGRESS;
+
+            pthread_mutex_unlock(&locality->local_lock);
+            void *res = task->func(task->pool, task->data);
+            pthread_mutex_lock(&locality->local_lock);
+
+            task->result = res;
+            task->status = COMPLETED;
+            pthread_cond_signal(&task->task_ready);
+        }
+        pthread_mutex_unlock(&locality->local_lock);
+        return task->result;    
+    }
     return task->result;
 }
 
@@ -230,42 +243,85 @@ void future_free(struct future *task)
 
 static void *worker_function(void *_arg)
 {
-    // Initialize worker thread values
-    struct worker_thread *worker = malloc(sizeof(*worker));
-    worker->pool = _arg;
-    worker->id = worker->pool->count;
-    worker->is_stopped = false;
-    worker->local_q_size = 0;
-    pthread_mutex_init(&worker->local_lock, NULL);
-
+    struct worker_thread *worker = _arg;
     list_init(&worker->local_q);
+    pthread_mutex_init(&worker->local_lock, NULL);
+    locality = worker;
 
-    pthread_mutex_lock(&worker->pool->global_lock);
-    //(worker->pool->threads[worker->pool->count]) = *worker;
-
+    pthread_barrier_wait(&worker->pool->readysetgo);
     while (!worker->pool->is_shutdown)
-    {
-        if (list_empty(&worker->pool->global_q))
+    {    
+        pthread_mutex_lock(&worker->local_lock);
+        if (!list_empty(&worker->local_q))
         {
-            pthread_cond_wait(&worker->pool->global_queue_not_empty, &worker->pool->global_lock);
-        }
-        else
-        {
-            struct future *f = list_entry(list_pop_front(&worker->pool->global_q), struct future, elem);
-            //worker->pool->global_q_size--;
+            struct future *f = list_entry(list_pop_back(&worker->local_q), struct future, elem);
 
             f->status = PROGRESS;
+
+            pthread_mutex_unlock(&worker->local_lock);
+            void *res = f->func(f->pool, f->data);
+            pthread_mutex_lock(&worker->local_lock);
+
+            f->result = res;
+            f->status = COMPLETED;
+            pthread_cond_signal(&f->task_ready);
+            pthread_mutex_unlock(&worker->local_lock);
+        }
+        else if (pthread_mutex_unlock(&worker->local_lock) == 0 && pthread_mutex_lock(&worker->pool->global_lock) == 0 && !list_empty(&worker->pool->global_q))
+        {
+            struct future *f = list_entry(list_pop_front(&worker->pool->global_q), struct future, elem);
+
+            f->status = PROGRESS;
+
             pthread_mutex_unlock(&worker->pool->global_lock);
             void *res = f->func(f->pool, f->data);
             pthread_mutex_lock(&worker->pool->global_lock);
 
             f->result = res;
             f->status = COMPLETED;
+
             pthread_cond_signal(&f->task_ready);
+            pthread_mutex_unlock(&worker->pool->global_lock);
+        }
+        else
+        {
+            pthread_mutex_unlock(&worker->pool->global_lock);
+            for (int idx = 0; idx < worker->pool->num_threads; idx++)
+            {
+                if (&worker->pool->threads[idx] != worker)
+                {
+                    pthread_mutex_lock(&worker->pool->threads[idx].local_lock);
+                    if (!list_empty(&worker->pool->threads[idx].local_q))
+                    {
+                        struct future *f = list_entry(list_pop_front(&worker->pool->threads[idx].local_q), struct future, elem);
+                        f->status = PROGRESS;
+
+                        pthread_mutex_unlock(&worker->pool->threads[idx].local_lock);
+                        void *res = f->func(f->pool, f->data);
+                        pthread_mutex_lock(&worker->pool->threads[idx].local_lock);
+    
+                        f->result = res;
+                        f->status = COMPLETED;
+
+                        pthread_cond_signal(&f->task_ready);
+                        pthread_mutex_unlock(&worker->pool->threads[idx].local_lock);
+                        continue;
+                    }
+                    pthread_mutex_unlock(&worker->pool->threads[idx].local_lock);
+                }
+            }
+            pthread_mutex_lock(&worker->pool->global_lock);
+            if (worker->pool->is_shutdown && list_empty(&worker->pool->global_q))
+            {
+                pthread_mutex_unlock(&worker->pool->global_lock);
+                break;
+            }
+            else
+            {
+                pthread_cond_wait(&worker->pool->global_queue_not_empty, &worker->pool->global_lock);
+                pthread_mutex_unlock(&worker->pool->global_lock);
+            }
         }
     }
-
-    pthread_mutex_unlock(&worker->pool->global_lock);
-
     return NULL;
-}
+}   
